a66570286db7e2fd79aa1e5aa3c706029cfd22fa
l <l>
2021-11-13 18:35:04 +0500

    Реализовал проверку у всех фигур

--- Chess.Logic/Figure.cs
+++ Chess.Logic/Figure.cs
@@ -13,7 +13,7 @@ namespace Chess.Logic
 
 		//public bool isWhite;
 
-		public abstract bool IsMoveCorrect();
+		public abstract bool IsMoveCorrect((int y, int x) pos1, (int y, int x) pos2);
 
 		public char ToChar()
 		{
@@ -24,6 +24,11 @@ namespace Chess.Logic
 		{
 			return MemberwiseClone();
 		}
+
+		public static int CalculateDelta(int n1, int n2)
+		{
+			return Math.Abs(n1 - n2);
+		}
 	}
 
 	public class Pawn : Figure
@@ -31,9 +36,12 @@ namespace Chess.Logic
 		public override char OneLetter => 'P';
 		public override string Name => "Пешка";
 
-		public override bool IsMoveCorrect()
+		public override bool IsMoveCorrect((int y, int x) pos1, (int y, int x) pos2)
 		{
-			throw new NotImplementedException();
+			int deltaX = CalculateDelta(pos1.x, pos2.x);
+			int deltaY = CalculateDelta(pos2.y, pos1.y);
+
+			return (deltaX == 0 && (deltaY == 1 || deltaY == 2));
 		}
 	}
 
@@ -42,9 +50,12 @@ namespace Chess.Logic
 		public override char OneLetter => 'B';
 		public override string Name => "Слон";
 
-		public override bool IsMoveCorrect()
+		public override bool IsMoveCorrect((int y, int x) pos1, (int y, int x) pos2)
 		{
-			throw new NotImplementedException();
+			int deltaX = CalculateDelta(pos1.x, pos2.x);
+			int deltaY = CalculateDelta(pos2.y, pos1.y);
+
+			return (deltaX ==  deltaY && deltaX != 0 && deltaY != 0);
 		}
 	}
 
@@ -53,9 +64,14 @@ namespace Chess.Logic
 		public override char OneLetter => 'N';
 		public override string Name => "Конь";
 
-		public override bool IsMoveCorrect()
+		public override bool IsMoveCorrect((int y, int x) pos1, (int y, int x) pos2)
 		{
-			throw new NotImplementedException();
+			int deltaX = CalculateDelta(pos1.x, pos2.x);
+			int deltaY = CalculateDelta(pos2.y, pos1.y);
+
+			return (
+				(deltaX == 2 || deltaY == 2) && (deltaX == 1 || deltaY == 1)
+				);
 		}
 	}
 
@@ -64,9 +80,14 @@ namespace Chess.Logic
 		public override char OneLetter => 'R';
 		public override string Name => "Ладья";
 
-		public override bool IsMoveCorrect()
+		public override bool IsMoveCorrect((int y, int x) pos1, (int y, int x) pos2)
 		{
-			throw new NotImplementedException();
+			int deltaX = CalculateDelta(pos1.x, pos2.x);
+			int deltaY = CalculateDelta(pos2.y, pos1.y);
+
+			return (
+				(deltaX == 0 || deltaY == 0) && (deltaX != 0 || deltaY != 0)
+				);
 		}
 	}
 
@@ -75,9 +96,14 @@ namespace Chess.Logic
 		public override char OneLetter => 'Q';
 		public override string Name => "Королева";
 
-		public override bool IsMoveCorrect()
+		public override bool IsMoveCorrect((int y, int x) pos1, (int y, int x) pos2)
 		{
-			throw new NotImplementedException();
+			int deltaX = CalculateDelta(pos1.x, pos2.x);
+			int deltaY = CalculateDelta(pos2.y, pos1.y);
+
+			return ((deltaX != 0 && deltaY !=0) &&
+					deltaX == deltaY ||
+					((deltaX == 0 || deltaY == 0) && (deltaX != 0 || deltaY != 0)));
 		}
 	}
 
@@ -86,9 +112,12 @@ namespace Chess.Logic
 		public override char OneLetter => 'K';
 		public override string Name => "Король";
 
-		public override bool IsMoveCorrect()
+		public override bool IsMoveCorrect((int y, int x) pos1, (int y, int x) pos2)
 		{
-			throw new NotImplementedException();
+			int deltaX = CalculateDelta(pos1.x, pos2.x);
+			int deltaY = CalculateDelta(pos2.y, pos1.y);
+
+			return (deltaX == 1 || deltaY == 1);
 		}
 	}
 }
